93d5a745ae35275d53bc52950721fe8e5a42fca7
yanyu <373349912@qq.com>
2021-03-01 15:20:14 +0800

    消防车辆返回修改

--- src/main/resources/static/js/appjs/jczy/xfcl/edit.js
+++ src/main/resources/static/js/appjs/jczy/xfcl/edit.js
@@ -67,7 +67,7 @@ function update() {
         type : "POST",
         url : "/jczy/xfcl/update",
         success : function(data) {
-            if (data.code == 0) {
+            if (data.code != "") {
                 parent.layer.msg("操作成功");
                 parent.reLoad();
                 var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
