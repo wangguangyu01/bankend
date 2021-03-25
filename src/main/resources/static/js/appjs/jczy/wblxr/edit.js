b66253af14aa33a6415e541128f2f9e6ac9a8d15
liangshengli <liangsl@raysdata.com>
2021-03-25 16:13:37 +0800

    修改外部联系人保存

--- src/main/resources/static/js/appjs/jczy/wblxr/edit.js
+++ src/main/resources/static/js/appjs/jczy/wblxr/edit.js
@@ -9,29 +9,20 @@ $.validator.setDefaults({
 	}
 });
 function update() {
-	$.ajax({
-		cache : true,
-		type : "POST",
-		url : "/jczy/wblxr/update",
-		data : $('#signupForm').serialize(),// 你的formid
-		async : false,
-		error : function(request) {
-			parent.layer.alert("Connection error");
-		},
-		success : function(data) {
-			if (data.code == 0) {
-				parent.layer.msg("操作成功");
-				parent.reLoad();
-				var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
-				parent.layer.close(index);
-
-			} else {
-				parent.layer.alert(data.msg)
-			}
-
-		}
-	});
-
+    $("#signupForm").ajaxSubmit({
+        type : "POST",
+        url : "/jczy/wblxr/update",
+        success : function(data) {
+            if (data.code != "") {
+                parent.layer.msg("操作成功");
+                parent.reLoad();
+                var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+                parent.layer.close(index);
+            } else {
+                parent.layer.alert(data.msg)
+            }
+        }
+    })
 }
 function validateRule() {
 	var icon = "<i class='fa fa-times-circle'></i> ";
