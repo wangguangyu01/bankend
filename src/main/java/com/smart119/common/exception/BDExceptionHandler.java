9a5224585d6a5aec4c88ea1004f26579f30bbd69
Fx <>
2021-04-13 10:37:59 +0800

    添加参数校验配置

--- src/main/java/com/smart119/common/exception/BDExceptionHandler.java
+++ src/main/java/com/smart119/common/exception/BDExceptionHandler.java
@@ -5,17 +5,23 @@ import com.smart119.common.domain.LogDO;
 import com.smart119.common.service.LogService;
 import com.smart119.common.utils.HttpServletUtils;
 import com.smart119.common.utils.R;
+import com.smart119.common.utils.ResultCode;
 import com.smart119.common.utils.ShiroUtils;
 import com.smart119.system.domain.UserDO;
 import org.apache.shiro.authz.AuthorizationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.util.Date;
 
 /**
@@ -79,5 +85,50 @@ public class BDExceptionHandler {
             return R.error(500, "服务器错误，请联系管理员");
         }
         return new ModelAndView("error/500");
+    }
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public Object MethodArgumentNotValidHandler(MethodArgumentNotValidException exception){
+        //按需重新封装需要返回的错误信息
+        StringBuilder msg = new StringBuilder();
+        //解析原错误信息，封装后返回，此处返回非法的字段名称，原始值，错误信息
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            msg.append(error.getDefaultMessage()).append(",");
+        }
+
+        if (msg.length() > 0) {
+            msg.deleteCharAt(msg.length() - 1);
+        }
+        return R.error(ResultCode.PARAM_IS_INVALID.code(), msg.toString());
+    }
+    //处理Get请求中 使用@Valid 验证路径中请求实体校验失败后抛出的异常，详情继续往下看代码
+    @ExceptionHandler(BindException.class)
+    public Object BindExceptionHandler(BindException exception) {
+        //按需重新封装需要返回的错误信息
+        StringBuilder msg = new StringBuilder();
+        //解析原错误信息，封装后返回，此处返回非法的字段名称，原始值，错误信息
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            msg.append(error.getDefaultMessage()).append(",");
+        }
+
+        if (msg.length() > 0) {
+            msg.deleteCharAt(msg.length() - 1);
+        }
+        return R.error(ResultCode.PARAM_IS_INVALID.code(), msg.toString());
+    }
+
+    //处理请求参数格式错误 @RequestParam上validate失败后抛出的异常是javax.validation.ConstraintViolationException
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Object ConstraintViolationExceptionHandler(ConstraintViolationException exception) {
+        //按需重新封装需要返回的错误信息
+        StringBuilder msg = new StringBuilder();
+        //解析原错误信息，封装后返回，此处返回非法的字段名称，原始值，错误信息
+        for (ConstraintViolation error : exception.getConstraintViolations()) {
+            msg.append(error.getMessage()).append(",");
+        }
+
+        if (msg.length() > 0) {
+            msg.deleteCharAt(msg.length() - 1);
+        }
+        return R.error(ResultCode.PARAM_IS_INVALID.code(), msg.toString());
     }
 }
---
+++ src/main/java/com/smart119/common/validated/Update.java
@@ -0,0 +1,10 @@
+package com.smart119.common.validated;
+
+/**
+ * @program: thrz
+ * @description: ${description}
+ * @author: sdw
+ * @create: 2021-02-25 11:43
+ **/
+public interface Update {
+}
