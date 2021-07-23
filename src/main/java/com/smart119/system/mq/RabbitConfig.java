fceaffd82558ff83c5d7390e3ff21b548b08d215
zhangsh <zhangsh@raysdata.com>
2021-07-23 16:29:54 +0800

    提交报错的文件。
    Change-Id: Icb42c5a3757837b9b93d5f4077bbdb25184d667e

--- src/main/java/com/smart119/system/mq/RabbitConfig.java
+++ src/main/java/com/smart119/system/mq/RabbitConfig.java
@@ -7,29 +7,28 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource("classpath:application-dev.yml")
 @ConfigurationProperties(prefix = "rabbitmq")
 public class RabbitConfig {
 
-    @Value("${host}")
-    private String serverIp;
-
-    @Value("${mq.port}")
-    private int port;
-
-    @Value("${mq.username}")
-    private String username;
-
-    @Value("${mq.password}")
-    private String password;
-
-
-    public ConnectionFactory getConnection(){
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(serverIp);
-        factory.setPort(port);
-        factory.setUsername(username);
-        factory.setPassword(password);
-        return factory;
-    }
+//    @Value("${host}")
+//    private String serverIp;
+//
+//    @Value("${mq.port}")
+//    private int port;
+//
+//    @Value("${mq.username}")
+//    private String username;
+//
+//    @Value("${mq.password}")
+//    private String password;
+//
+//
+//    public ConnectionFactory getConnection(){
+//        ConnectionFactory factory = new ConnectionFactory();
+//        factory.setHost(serverIp);
+//        factory.setPort(port);
+//        factory.setUsername(username);
+//        factory.setPassword(password);
+//        return factory;
+//    }
 }
