e7a6979cfb62fd740424fc1c542e873e15d07447
wangguangyu <447572324@qq.com>
2021-11-05 11:34:29 +0800

    修改天然水源查询

--- src/main/resources/static/js/appjs/jczy/trsy/trsy.js
+++ src/main/resources/static/js/appjs/jczy/trsy/trsy.js
@@ -35,8 +35,9 @@ function load() {
                         //说明：传入后台的参数包括offset开始索引，limit步长，sort排序列，order：desc或者,以及所有列的键值对
                         limit: params.limit,
                         offset: params.offset,
-                        deptId: zid
-                        // name:$('#searchName').val(),
+                        deptId: zid,
+                        mc: $.trim($('#mc').val()),
+                        status: $.trim($('#status').val())
                         // username:$('#searchName').val()
                     };
                 },
