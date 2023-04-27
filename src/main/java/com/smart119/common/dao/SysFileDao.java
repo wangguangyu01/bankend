73b982b354c2a67e792864f9436b21481f9cd8f3
wangguangyu0303 <874738684@qq.com>
2023-04-27 19:33:55 +0800

    后管

---
+++ src/main/java/com/smart119/common/dao/SysFileDao.java
@@ -0,0 +1,7 @@
+package com.smart119.common.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.smart119.common.domain.SysFile;
+
+public interface SysFileDao extends BaseMapper<SysFile> {
+}
